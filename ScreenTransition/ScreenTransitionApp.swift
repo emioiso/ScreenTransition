42fc94628a9c3d174731b93fe7ab44ae85edaac9
emioiso <jijjjiijiijijiji@gmail.com>
2023-10-01 10:34:26 +0900

    Initial Commit

---
+++ ScreenTransition/ScreenTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenTransitionApp.swift
+//  ScreenTransition
+//
+//  Created by emi oiso on 2023/10/01.
+//
+
+import SwiftUI
+
+@main
+struct ScreenTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
